e4817ee76834ecf768bd22822bf0de5749385e1c
alvydas spokauskas <alspok@gmail.com>
2022-05-07 23:42:25 +0300

    .

--- Classes/InitValues.py
+++ Classes/InitValues.py
@@ -17,6 +17,7 @@ class InitValues():
     # taxon_name = "Dikarya"
     
     path = "C:\\Users\\hp\\source\\repos\\DinucFrames"
+    path = os.path.getcwd()
     
     # seq_path = "C:\\Users\\hp\\source\\repos\\Sequencies\\Procaryote\\Plasmids\\"
     seq_path = os.getcwd()
