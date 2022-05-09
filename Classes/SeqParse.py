4fdd5b6e7069ace954b3f44a0b308cb03e2971a6
alvydas <alspok@gmail.com>
2022-05-09 23:18:02 +0300

    .

--- Classes/SeqParse.py
+++ Classes/SeqParse.py
@@ -17,22 +17,27 @@ class SeqParse():
                             "jahley", "jaal", "jabw",
                             "jabmlo", "jahhpk", "jaiwoz",
                             "jakvpt", "jafkak", "jaifh",
-                            "jaiwqd", "jaifhz",
+                            "jaiwqd", "jaifhz", "jahne",
+                            "jagks",
                             "ltoo",
                             "nw",
-                            "smom", 
-                            "rrcj",
+                            "pesf"
+                            "rrcj", "rcwq",
+                            "smom", "sthe", "sthd",
+                            "sthb", "sthf" #last
                             "vahf",
                             "wnkj", "wnkj", "wbvu",
-                            
                         ]
+        exept_seq_length = 50000
+
         for seq_file in seq_files:
             with open(f".\\temp\\{seq_file}", 'r') as seqfh:
                 for seq_record in SeqIO.parse(seqfh, "fasta"):
                     desc = seq_record.description.lower()
                     id = seq_record.id.lower()
                     if (any(item in desc for item in exept_description_list) or
-                        any(item in id for item in exept_id_list)):
+                        any(item in id for item in exept_id_list) or
+                        seq_record.seq < exept_seq_length):
                         continue
                     else:
                         yield seq_record
