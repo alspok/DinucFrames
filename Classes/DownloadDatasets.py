31260c64393a8bd36e906efeb29c2427c8e38e8d
alvydas spokauskas <alspok@gmail.com>
2022-05-10 10:05:00 +0300

    .

--- Classes/DownloadDatasets.py
+++ Classes/DownloadDatasets.py
@@ -1,15 +1,17 @@
 import requests
+import os
 
 class DownloadDatasets():
     def __init__(self) -> None:
         pass
     
     def downloadDatasets(self) -> None:
+        os.chdir("..\\")
         try:
             print("Downloading datasets.exe...")
             url = 'https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/win64/datasets.exe'
             req = requests.get(url, allow_redirects=True)
-            open('.\\bin\\datasets.exe', 'wb').write(req.content)
+            open(".\\bin\\datasets.exe", "wb").write(req.content)
         except Exception as e:
             print(f"Datasets download error. {e}")
             pass
