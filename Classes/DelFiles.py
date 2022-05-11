dc4de23271f92a61b4b9d4bcb7d6f35a34171605
alvydas spokauskas <alspok@gmail.com>
2022-05-11 19:20:48 +0300

    .

--- Classes/DelFiles.py
+++ Classes/DelFiles.py
@@ -9,10 +9,5 @@ class DelFiles():
             path = f"{cwd}\\temp"
             os.chdir(path)
             for file in os.listdir(path):
-<<<<<<< HEAD
                 os.remove(file)
-=======
-                    os.remove(file)
-        
->>>>>>> 56325930bd488dd8a888bec1e8cf20de2ac9abb5
         pass
